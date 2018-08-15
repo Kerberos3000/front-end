715c0a6d69b9a413aedd1324517a57721841550e
Philipp Meyer <meyer.github@gmail.com>
2018-08-15 09:40:08 +0200

    Point to API Gateway

--- api/endpoints.js
+++ api/endpoints.js
@@ -15,14 +15,14 @@
   });
 
   module.exports = {
-    catalogueUrl:  util.format("http://catalogue%s", domain),
-    tagsUrl:       util.format("http://catalogue%s/tags", domain),
-    cartsUrl:      util.format("http://carts%s/carts", domain),
-    ordersUrl:     util.format("http://orders%s", domain),
-    customersUrl:  util.format("http://user%s/customers", domain),
-    addressUrl:    util.format("http://user%s/addresses", domain),
-    cardsUrl:      util.format("http://user%s/cards", domain),
+    catalogueUrl:  util.format("http://api/catalogue%s", domain),
+    tagsUrl:       util.format("http://api/catalogue%s/tags", domain),
+    cartsUrl:      util.format("http://api/carts%s/carts", domain),
+    ordersUrl:     util.format("http://api/orders%s", domain),
+    customersUrl:  util.format("http://api/user%s/customers", domain),
+    addressUrl:    util.format("http://api/user%s/addresses", domain),
+    cardsUrl:      util.format("http://api/user%s/cards", domain),
     loginUrl:      util.format("http://user%s/login", domain),
-    registerUrl:   util.format("http://user%s/register", domain),
+    registerUrl:   util.format("http://api/user%s/register", domain),
   };
 }());
